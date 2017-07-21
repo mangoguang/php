16e0c370775e3c0dd4f22ff1fadef0fa8b3764f4
mangoguang <542436458@qq.com>
2017-07-21 12:26:28 +0800

    add login.php

--- port/login.php
+++ port/login.php
@@ -1,6 +1,10 @@
 <?php
 $name=$_POST["name"];
 $password=$_POST["password"];
-$data = array("name"=>$name,'password'=>$password);
+if($name == 'mangoguang'&&$password == 123456){
+  $data = array("name"=>$name,'status'=>'success');
+}else{
+  
+}
 print_r(json_encode($data));
 ?> 
